378ccb384b8ada1dc78eee56cad4cce45613c378
nayarmegha <me@meghanayar.com>
2022-11-06 13:25:21 -0600

    added pyth tut

--- src/index.js
+++ src/index.js
@@ -5,22 +5,19 @@ import './index.css'
 const App = (props) => {
   return (
       <div className='frame22'>
-        <span id = "cloud-circle1"
-        />
+        <span id = "cloud-circle1"/>
         <span className='text'>
-          <span>
             <span> <a href = "https://www.linkedin.com/in/meghadnayar/"> Linkedin </a></span>
             <br></br>
             <br></br>
             <span><a href="https://github.com/nayarmegha"> GitHub </a></span> 
-          </span>
         </span>
         <span className='about'>
             I’m a student programmer and designer based in Chicago. <br></br>
-            Check out some of my&nbsp;
-          <span className='aboutlinks'><a href = "https://github.com/nayarmegha">projects here</a></span>
-          <span className='about2'>
-            .&nbsp;If you have any questions or would like to contact me,
+            Currently, I'm building a <span className='aboutlinks'><a href="https://incandescent-gingersnap-9afa2e.netlify.app/index.html"> gamified Python tutorial</a></span>, with an intention to make Python feel more accesible for beginners to the language. 
+            <br></br>
+            Check out some of my other<span className='aboutlinks'><a href = "https://github.com/nayarmegha"> projects here.</a></span>
+          <span className='about2'> If you have any questions or would like to contact me,
           </span>
           <span className='aboutlinks'> <a href = "mailto: meghadnay08@gmail.com" > hit me up here. </a></span>
         </span>
